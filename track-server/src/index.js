29a4f3ef46b7094c994fd18a8688e23ac51db996
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-01-26 17:16:59 -0500

    implemented connection to MongoDB instance

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-xjynx.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true
+  ,useCreateIndex: true
+  ,useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo instance boiii');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to Mongo: ', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
