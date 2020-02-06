3a3152b450b5ea338a8660b4eb6f9327fecce8af
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-02-05 23:47:39 -0500

    started inital boilerplate for app

---
+++ tracks/App.js
@@ -0,0 +1,6 @@
+import AccountScreen from './src/screens/AccountScreen';
+import SinginScreen from './src/screens/SinginScreen';
+import SingupScreen from './src/screens/SingupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
