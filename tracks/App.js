d257c1181a808dc5974195ef3ed8708d95541653
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-02-16 16:52:27 -0500

    implemented scaffolding of navigation flow

--- tracks/App.js
+++ tracks/App.js
@@ -1,6 +1,29 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import AccountScreen from './src/screens/AccountScreen';
-import SinginScreen from './src/screens/SinginScreen';
-import SingupScreen from './src/screens/SingupScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+
+// this mimics the flow of our app navigators
+// case naming convention by choice differentiates screens and flows
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen
+    ,Signin: SigninScreen
+  })
+  ,mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen
+      ,TrackDetail: TrackDetailScreen
+    })
+    ,TrackCreate: TrackCreateScreen
+    ,Account: AccountScreen
+  })
+});
+
+export default createAppContainer(switchNavigator);
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,1 +1,16 @@
-SignupScreen.js
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const SignupScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button title="Go to Sign In" onPress={() => navigation.navigate('Signin')} />
+      <Button title="Go to Main Flow" onPress={() => navigation.navigate('mainFlow')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
