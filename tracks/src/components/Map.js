563f9e806739567fde7b3416214fef7c34076738
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-03-02 22:05:07 -0500

    location updates are now tracked by map component. map moves with current location

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
+
 
 // MapView component is very similar to an image in that it has no dimension unless assigned
 const Map = () => {
-  // temp code only for testing
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      points.push({
-        latitude: 37.33233 + i * 0.001
-        ,longitude: -122.03121 + i * 0.001
-      });
-    } else {
-      points.push({
-        latitude: 37.33233 - i * 0.002
-        ,longitude: -122.03121 + i * 0.001
-      });
-    }
+  const { state: { currentLocation } } = useContext(LocationContext);
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
   }
+
+  // temp code only for testing
+  // let points = [];
+  // for (let i = 0; i < 20; i++) {
+  //   if (i % 2 === 0) {
+  //     points.push({
+  //       latitude: 37.33233 + i * 0.001
+  //       ,longitude: -122.03121 + i * 0.001
+  //     });
+  //   } else {
+  //     points.push({
+  //       latitude: 37.33233 - i * 0.002
+  //       ,longitude: -122.03121 + i * 0.001
+  //     });
+  //   }
+  // }
 
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 43.717798
-        ,longitude: -79.803029
+        ...currentLocation.coords
+        ,latitudeDelta: 0.01
+        ,longitudeDelta: 0.01
+      }}
+      region={{
+        ...currentLocation.coords
         ,latitudeDelta: 0.01
         ,longitudeDelta: 0.01
       }}
     >
-      <Polyline coordinates={points} />
     </MapView>
   );
 };
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [error, setError] = useState(null);
 
   const startWatching = async() => {
@@ -17,7 +19,7 @@ const TrackCreateScreen = () => {
         ,timeInterval: 1000 // once every second
         ,distance: 10 // or every 10 metres
       }, (location) => {
-        // console.log(location);
+        addLocation(location);
       });
     } catch (err) {
       setError(err);
