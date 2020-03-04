fc793e5e26d100d52046888143430ce4262cb261
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-03-04 00:17:13 -0500

    initial hook up of track form logic. not totally working

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = () => {
+  const { state: { name, recording, locations } , startRecording, stopRecording, changeName } = useContext(LocationContext);
+
+  console.log(locations.length);
+
   return (
     <View>
       <Spacer>
-        <Input placeholder="Enter Name" />
+        <Input value={name} onChangeText={changeName} placeholder="Enter Name" />
       </Spacer>
-     <Button title="Start Recording" />
+      {recording ? (
+        <Button title="Stop" onPress={stopRecording} />
+      ) : (
+        <Button title="Start Recording" onPress={startRecording} />
+      )}
     </View>
   );
 };
