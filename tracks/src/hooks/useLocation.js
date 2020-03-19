a15567f6173b00a3996ec33a0e6f6e561879e12e
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-03-18 21:34:58 -0400

    tips to avoid stale references

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -3,32 +3,41 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-loca
 
 export default (shouldTrack, callback) => {
   const [error, setError] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
+  // const [subscriber, setSubscriber] = useState(null);
 
-  const startWatching = async() => {
-    try {
-      await requestPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation  // more accuracy means more bettery consumption
-          ,timeInterval: 1000 // once every second
-          ,distance: 10 // or every 10 metres
-        }
-        ,callback
-      );
-
-      setSubscriber(sub);
-    } catch (err) {
-      setError(err);
-    }
-  };
-
+  // to help avoid stale references, useEffect should never reference state, props, or context values
+  // without adding them to the dependency list.
+  // 'setter' functions for state values are okay to reference
   useEffect(() => {
+    let subscriber;
+    // generally not good to define helper functions (like startWatching)
+    // that reference state, props, or context values as they may change over time and introduce bugs
+    // better to define them inside of useEffect so we can easily see the references we should
+    // add to the useEffect dependency list
+    const startWatching = async() => {
+      try {
+        await requestPermissionsAsync();
+        const subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation  // more accuracy means more bettery consumption
+            ,timeInterval: 1000 // once every second
+            ,distance: 10 // or every 10 metres
+          }
+          ,callback
+        );
+      } catch (err) {
+        setError(err);
+      }
+    };
+
+
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
     return () => {
