3934c2c2c87d1c15a8bfcd1e3b2431041004594d
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2020-03-22 23:23:10 -0400

    added polyline when hitting record

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://1facf329.ngrok.io'
+  baseURL: 'http://658dc9b0.ngrok.io'
 });
--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -6,7 +6,7 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 // MapView component is very similar to an image in that it has no dimension unless assigned
 const Map = () => {
-  const { state: { currentLocation } } = useContext(LocationContext);
+  const { state: { currentLocation, locations } } = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
@@ -27,6 +27,7 @@ const Map = () => {
         strokeColor="rgba(158, 158, 255, 1.0)"
         fillColor="rgba(158, 158, 255, 0.3)"
       />
+      <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
   );
 };
